da36fbf24a32788f648f68963c574de19b450fdf
Igor <igordyb@gmail.com>
2024-10-27 19:15:32 +0100

    Basic mechanics of the program

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import pygame
+import pygame as pg
 import random
 import sys
 
-pygame.init()
-pygame.font.init()
+from pygame.examples.moveit import WIDTH, HEIGHT
+
+pg.init()
+pg.font.init()
+font = pg.font.SysFont("comicsansms", 80)
 
 # Constants
 SCREEN_WIDTH, SCREEN_HEIGHT = 400, 650
@@ -12,24 +15,48 @@ WHITE = (255, 255, 255)
 GREY = (200, 200, 200)
 RED = (255, 0, 0)
 
-# Game variables
-num_target = 0
-user_input = 0
-attempts = 0
-
 def main():
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    num_target = 0
+    user_input = ""
+    attempts = 0
+    input_box = pg.Rect(100, 100, 140, 32)
+    color_inactive = pg.Color('lightskyblue3')
+    color_active = pg.Color('dodgerblue2')
+    color = color_inactive
+    active = False
     running = True
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     while running:
-        screen.fill(BLACK)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:  # Correctly check for QUIT event
+        num_target = random.randint(1, 10)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:  # Correctly check for QUIT event
                 running = False
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if input_box.collidepoint(event.pos):
+                    active = not active
+                else:
+                    active = False
+                color = color_active if active else color_inactive
+            elif event.type == pg.KEYDOWN:
+                if chr(event.key).isdigit():
+                    user_input += chr(event.key)
+                if event.key == pg.K_RETURN:
+                    print(f'Output {user_input}')
+                    user_input = int(user_input)
+                    if user_input == num_target:
+                        attempts += 1
+                        print(f'You guessed correctly! {num_target} was the correct number!')
+                        print(f'It took you {attempts} tries to get the number right')
+                    else:
+                        print("You guessed wrong! Try again")
+                        attempts += 1
+                    user_input = ""
 
-        pygame.display.flip()
 
-    pygame.quit()
+        screen.fill(BLACK)
+        pg.display.flip()
+
+    pg.quit()
     sys.exit()
 
 
